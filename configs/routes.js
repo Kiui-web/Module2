3ad66463967ec44cf8770ed3415ee35ad599983c
aitorgb <aitorguerrero9@gmail.com>
2020-08-26 18:35:46 +0200

    Feat: server ok

--- configs/routes.js
+++ configs/routes.js
@@ -1,1 +1,12 @@
+const express = require('express')
+const router = express.Router()
+const userController = require ('../controllers/users.controller')
+const eventController = require('../controllers/event.controller')
+const sessionController = require('../middlewares/session.middleware')
+const uploads = require('../configs/cloudinary.config')
+
+router.get('/', userController.index)
+
+
+module.exports = router;
 
