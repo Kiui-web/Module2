3ea24ba5340989bb10c55c65ad93459a94a0c868
aitorgb <aitorguerrero9@gmail.com>
2020-09-13 17:23:13 +0200

    Feat : variable created

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const MONGODB_URI = 'mongodb://localhost/kiui-app'
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/kiui-app'
 
 mongoose.
   connect(MONGODB_URI, { 
